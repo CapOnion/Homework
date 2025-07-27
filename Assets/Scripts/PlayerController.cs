7b5b8aac0d58497df5b8896d306d4a057fe15fd2
Fox032 <45915171+CapOnion@users.noreply.github.com>
2025-07-28 00:08:26 +0300

    Added basic gameplay, bomb timer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,69 @@
-using Dreamteck.Forever;
+using System;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using Dreamteck.Forever;
 
 public class PlayerController : MonoBehaviour
 {
-    private InputControls _inputController;
+    [SerializeField] private AudioClip _explosionSound;
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TextMeshProUGUI _timeToExplodeText;
+    [SerializeField] private TextMeshProUGUI _gameOverText;
+    [SerializeField] private float timeToExplode = 60;
+     private InputControls _inputController;
     [SerializeField] private Runner _basicRunner;
     [SerializeField] private float _slideSpeed = 5f;
     [SerializeField] private int _levelWidth = 5;
+
+
+    private int scoreNumber = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            scoreNumber += 10;
+            Destroy(other.gameObject);
+            AudioSource.PlayClipAtPoint(_explosionSound, transform.position);
+            _scoreText.text = "Score: " + scoreNumber;
+        }
+    }
+
+    private void Start()
+    {
+        _gameOverText.enabled = false;
+    }
+    private void Update()
+    {
+        var finalOffset = UnityEngine.Vector2.MoveTowards(_basicRunner.motion.offset, _targetVector, _slideSpeed * Time.deltaTime);
+        _basicRunner.motion.offset = finalOffset;
+        
+        timeToExplode -= 1 * Time.deltaTime;
+        _timeToExplodeText.text = "Time to explode: " + timeToExplode;
+
+        if (timeToExplode <= 0)
+        {
+            scoreNumber -= 100;
+            if (scoreNumber <= 0)
+            {
+                GameOver();
+            }
+            //ResetTimer();
+        }
+    }
+
+    private void ResetTimer()
+    {
+        timeToExplode = 60;
+    }
+
+    private void GameOver()
+    {
+        _gameOverText.enabled = true;
+        _basicRunner.follow = false;
+    }
 
     private Vector2 _targetVector;
     private void Awake()
@@ -29,26 +86,14 @@ public class PlayerController : MonoBehaviour
 
     private void OnMovementRecieved(Vector2 movement)
     {
-        Debug.Log("movement " + movement);
         _targetVector = new Vector2(movement.x * _levelWidth, _basicRunner.motion.offset.y);
-        // UpdateMovement(movement.x);
     }
 
     private void OnMovementEnd()
     {
-        _targetVector = Vector2.zero;
+        _targetVector = _basicRunner.motion.offset;
     }
 
-    // private void UpdateMovement(float _x)
-    // {
-    //     _basicRunner.motion.offset = new Vector2(_x, _basicRunner.motion.offset.y);
-    // }
-
-    private void Update()
-    {
-        var finalOffset = UnityEngine.Vector2.MoveTowards(_basicRunner.motion.offset, _targetVector, _slideSpeed * Time.deltaTime);
-        _basicRunner.motion.offset = finalOffset;
-    }
     private void OnDestroy()
     {
         UnsubscribeEvents();
